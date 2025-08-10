bef44060f7de0800faf8a9701a33b6a40b3bf74b
developerTamanna <astamanna57@gmail.com>
2025-08-10 13:30:06 +0600

    responsiveness

--- src/components/WebsiteServices.jsx
+++ src/components/WebsiteServices.jsx
@@ -42,7 +42,7 @@ export default function WebsiteServices() {
   ];
 
   return (
-    <section className="bg-white dark:bg-black text-black dark:text-white py-16 px-4 md:px-10 mt-10">
+    <section className="bg-white dark:bg-black text-black dark:text-white py-16 px-4 md:px-10 mt-6">
       <h2 className="text-3xl md:text-4xl font-bold text-center text-gray-900 dark:text-white mb-10">
         Our Website Services
       </h2>
